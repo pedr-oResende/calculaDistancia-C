1e9046c0f1ecfc5f6d90e2a6102cdf6359761be8
pedr-oResende <pedroresende61@gmail.com>
2021-03-03 00:15:44 -0300

    metodos implementados

--- main.c
+++ main.c
@@ -4,6 +4,69 @@
 #include <locale.h>
 #include <ctype.h>
 
+void clear_keyboard_buffer();
+void caractereEspecial();
+void printCidades(int);
+void preencheMatriz(int, int[][0]);
+int calculaDistancia(int,int[][0], char*, char*); // metodo que retorna a distancia total da rota
+void printResultado(int, char*);
+int indexOfCidade(char); // metodo transforma a cidade (char) em um numero correspondente na matriz
+
+int main()
+{
+    int numeroCidade;
+
+    caractereEspecial();
+    for(;;)
+    {
+        system("cls");
+        printf("Entre com a quantidade de cidades[3, 4, 5]: ");
+        scanf("%d", &numeroCidade);
+        if (numeroCidade >= 3 && numeroCidade <= 5)
+        {
+            break;
+        }
+        else
+        {
+            system("cls");
+            printf("Opção inválida! Entre com um número de cidades entre 3 e 5.\n\n");
+            system("pause");
+        }
+    }
+
+    int matriz[numeroCidade][numeroCidade];
+
+    preencheMatriz(numeroCidade, matriz);
+
+    for(;;) //Loop para o usuário entrar com quantas rotas quiser
+    {
+        char rota[30] = "";
+        char vetorSoma[10] = "";
+        int resposta;
+
+        system("cls");
+        printCidades(numeroCidade);
+        printf("\n\nEntre com a rota: ");
+        gets(rota);
+        do
+        {
+            printResultado(calculaDistancia(numeroCidade, matriz,vetorSoma,rota), vetorSoma);
+            printf("\n\n0 - Sair");
+            printf("\n\n1 - Escolher outra rota\n\n");
+            scanf("%d", &resposta);
+            clear_keyboard_buffer();
+            if (resposta == 0)
+            {
+                exit(1);
+            }
+        }
+        while(resposta != 1);
+
+    }
+}
+
+
+
 void clear_keyboard_buffer()
 {
     int c = 0;
@@ -13,17 +76,36 @@ void clear_keyboard_buffer()
 
 void caractereEspecial()
 {
-    printf("%s",setlocale(LC_ALL,"")); //funcao pra permitir caracteres especiais no programa
+    printf("%s",setlocale(LC_ALL,""));
     system("cls");
 }
-void printMenu()
+
+void preencheMatriz(int numeroCidade, int matriz[numeroCidade][numeroCidade])
 {
     system("cls");
-    printf("-------------CALCULAR DISTÂNCIA DE CIDADES-------------\n\n");
-    printf("0 - Sair");
-    printf("\n\n1 - Entrar com o número de cidades");
-    printf("\n\n2 - Entrar com a distância entre as cidades");
-    printf("\n\n3 - Entrar com a rota entre as cidades\n\n");
+    for (int i = 0; i < numeroCidade; i++)
+    {
+        for (int j = 0; j < numeroCidade; j++)
+        {
+            if (j > i) // condição para preencher somente da diagonal principal para cima
+            {
+                if (i == j)
+                {
+                    matriz[i][j] = 0;
+                }
+                else
+                {
+                    system("cls");
+                    printf("Distância entre as cidades %c e %c: ", (i + 65), (j + 65));
+                    scanf("%d", &matriz[i][j]);
+                    matriz[j][i] = matriz[i][j];
+                    clear_keyboard_buffer();
+
+                }
+            }
+        }
+
+    }
 }
 
 void printCidades(int numeroCidade)
@@ -35,10 +117,38 @@ void printCidades(int numeroCidade)
     }
 }
 
+int calculaDistancia(int numeroCidade,int matriz[numeroCidade][numeroCidade], char *vetorSoma, char *rota)
+{
+    char vetorAuxiliar[10] = "";
+    int soma = 0, count = 0;
+    for (int i = 0; i < numeroCidade; i++)
+    {
+        vetorAuxiliar[i] = i + 65;
+    }
+
+    for (int i = 0; i < strlen(rota); i++)
+    {
+        for (int j = 0; j < strlen(vetorAuxiliar); j++)
+        {
+            if (toupper(rota[i]) == vetorAuxiliar[j])
+            {
+                vetorSoma[count] = vetorAuxiliar[j];
+                if (count >= 1)
+                {
+                    soma += matriz[indexOfCidade(vetorSoma[count - 1])][indexOfCidade(vetorSoma[count])];
+                }
+                count++;
+                break;
+            }
+        }
+    }
+    return soma;
+}
+
 void printResultado(int distanciaTotal, char *vetorSoma)
 {
     system("cls");
-    printf("A distancia a ser percorrida entre as cidades:");
+    printf("A distancia a ser percorrida na rota entre as cidades:");
     for (int i = 0; i < strlen(vetorSoma); i++)
     {
         printf(" %c", vetorSoma[i]);
@@ -51,135 +161,4 @@ int indexOfCidade(char nomeCidade)
     return (int) nomeCidade - 65;
 }
 
-int main()
-{
-    int numeroCidade = 1, opcao, flag = 0, distanciaTotal = 0;
-
-    caractereEspecial();
-
-    for(;;)
-    {
-        int matriz[numeroCidade][numeroCidade];
-        printMenu();
-        scanf("%d\n", &opcao);
-        clear_keyboard_buffer();
-
-        switch ((int)opcao)
-        {
-        case 0:
-            exit(1);
-
-        case 1:
-            system("cls");
-            printf("Entre com a quantidade de cidades: ");
-            scanf("%d", &numeroCidade);
-            flag = 1;
-            break;
-
-        case 2:
-            if (flag == 1)
-            {
-                system("cls");
-
-                for (int i = 0; i < numeroCidade; i++)
-                {
-                    for (int j = 0; j < numeroCidade; j++)
-                    {
-                        if (i <= j) // condição para preencher somente da diagonal principal para cima
-                        {
-                            if (i == j)
-                            {
-                                matriz[i][j] = 0;
-                            }
-                            else
-                            {
-                                system("cls");
-                                printf("Distância entre as cidades %c e %c: ", (i + 65), (j + 65));
-                                scanf("%d\n", &matriz[i][j]);
-                                matriz[j][i] = matriz[i][j];
-                            }
-                        }
-                    }
-
-                }
-                flag = 2;
-            }
-            else
-            {
-                system("cls");
-                printf("ERRO! Número de cidades não preenchido.\n\n");
-                system("pause");
-            }
-            break;
-        case 3:
-            if (flag == 2)
-            {
-                int resposta;
-                do
-                {
-                    char vetorAuxiliar[10] = "";
-                    char rota[30] = "";
-                    char vetorSoma[10] = "";
-                    int count = 0;
-                    for (int i = 0; i < numeroCidade; i++)
-                    {
-                        vetorAuxiliar[i] = i + 65;
-                    }
-
-                    system("cls");
-                    printCidades(numeroCidade);
-                    printf("\n\nEntre com a rota: ");
-                    gets(rota);
-                    gets(rota);
-                    for (int i = 0; i < strlen(rota); i++)
-                    {
-                        for (int j = 0; j < strlen(vetorAuxiliar); j++)
-                        {
-                            if (toupper(rota[i]) == vetorAuxiliar[j])
-                            {
-                                vetorSoma[count] = vetorAuxiliar[j];
-                                if (count >= 1)
-                                {
-                                    distanciaTotal += matriz[indexOfCidade(vetorSoma[count - 1])][indexOfCidade(vetorSoma[count])];
-                                }
-                                count++;
-                                break;
-                            }
-                        }
-                    }
-                    printResultado(distanciaTotal, vetorSoma);
-
-                    printf("\n\n0 - Sair");
-                    printf("\n\n1 - Escolher outra rota");
-                    printf("\n\n2 - Voltar ao início\n\n");
-                    scanf("%d", &resposta);
-
-                }
-                while(resposta == 1);
-                switch (resposta)
-                {
-                case 0:
-                    exit(1);
-                case 2:
-                    flag = 0;
-                    break;
-                }
-
-            }
-
-            else
-            {
-                system("cls");
-                printf("ERRO! Distancia entre as cidades não preenchida.\n\n");
-                system("pause");
-            }
-            break;
-        default:
-            system("cls");
-            printf("ERRO! Opção inválida.\n\n");
-            system("pause");
-            break;
-        }
-    }
-}
 
